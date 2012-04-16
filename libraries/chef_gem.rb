7e26b4318d78e8d0ccf87fe6329f7f6acb71df5d
Chris Roberts <chrisroberts.code@gmail.com>
2012-04-16 08:57:31 -0700

    Install immediately. Clear paths after install to make new gem available.

--- libraries/chef_gem.rb
+++ libraries/chef_gem.rb
@@ -7,7 +7,24 @@ class Chef::Resource::GemPackage
 end
 
 class Chef::Resource::ChefGem < Chef::Resource::GemPackage
+
+  #  provides :chef_gem, :on_platforms => :all
+
+  def initialize(name, run_context=nil)
+    super
+    @resource_name = :chef_gem
+    @provider = Chef::Provider::Package::Rubygems
+    after_created
+  end
+
   def gem_binary(*args)
     node[:chef_gem_binary] || '/opt/opscode/embedded/bin/gem'
+  end
+
+  def after_created
+    Array(@action).flatten.compact.each do |action|
+      self.run_action(action)
+    end
+    Gem.clear_paths
   end
 end
